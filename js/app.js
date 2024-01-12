49a8632ecd3df631f460d4148bcd93e5b1139c11
Vasyl Yuriichuk <91080927+yuriichuk21@users.noreply.github.com>
2024-01-12 11:42:46 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,62 @@
-/* Put some script here */
+document.getElementById('add-button').addEventListener('click', function () {
+    performOperation('+');
+});
+
+document.getElementById('sub-button').addEventListener('click', function () {
+    performOperation('-');
+});
+
+document.getElementById('mul-button').addEventListener('click', function () {
+    performOperation('*');
+});
+
+document.getElementById('div-button').addEventListener('click', function () {
+    performOperation('/');
+});
+
+document.getElementById('log-button').addEventListener('click', function () {
+    performFunction('log');
+});
+
+document.getElementById('sin-button').addEventListener('click', function () {
+    performFunction('sin');
+});
+
+document.getElementById('tan-button').addEventListener('click', function () {
+    performFunction('tan');
+});
+
+function performOperation(operator) {
+    const op1 = parseFloat(document.getElementById('op1').value);
+    const op2 = parseFloat(document.getElementById('op2').value);
+
+    let result;
+    switch (operator) {
+        case '+':
+            result = op1 + op2;
+            break;
+        case '-':
+            result = op1 - op2;
+            break;
+        case '*':
+            result = op1 * op2;
+            break;
+        case '/':
+            result = op1 / op2;
+            break;
+    }
+
+    document.getElementById('res').textContent = 'Result: ' + result;
+}
+
+function performFunction(func) {
+    const op1 = parseFloat(document.getElementById('op1').value);
+    
+    const helpData = {
+        log: 'Natural logarithm (logarithm to the base e)',
+        sin: 'Sine of an angle in degrees',
+        tan: 'Tangent of an angle in degrees'
+    };
+
+    document.getElementById('content').textContent = helpData[func];
+}
